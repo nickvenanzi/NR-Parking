4728a2bbf61008decfc22016b2533a5a883bad3b
Nick Venanzi <nickvenanzi@gmail.com>
2024-10-16 19:04:30 -0400

    Initial Commit

---
+++ NavalReactorsParking/NavalReactorsParkingApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavalReactorsParkingApp.swift
+//  NavalReactorsParking
+//
+//  Created by Nick Venanzi on 10/16/24.
+//
+
+import SwiftUI
+
+@main
+struct NavalReactorsParkingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
